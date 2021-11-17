d744373e777eea042b23340b338823f8ae207dfa
masih moloodian <masihmoloodian@gmail.com>
2021-11-17 18:09:35 +0330

    - feat: Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true })
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('URL Shortener')
+    .setDescription('The URL Shortener API description')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('docs', app, document)
+  await app
+    .listen(Number(process.env.SERVICE_PORT), process.env.SERVICE_HOST)
+    .then(() =>
+      console.info(
+        `Server is running on http://${process.env.SERVICE_HOST}:${process.env.SERVICE_PORT}\n`,
+        `Swagger is running on http://${process.env.SERVICE_HOST}:${process.env.SERVICE_PORT}/docs`
+      )
+    )
 }
-bootstrap();
+bootstrap()
